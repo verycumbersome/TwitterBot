1ac68cbc7466f2c55029ba41581df375dd2ea82c
Matthew Jordan <matthewsages@gmail.com>
2020-06-10 17:05:01 -0700

    Update class functionality

--- main.py
+++ main.py
@@ -27,32 +27,25 @@ class Sentiment():
         inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
         self.vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
 
-        alpha = 1.4
 
     def train(self):
-        self.posText = []
-        self.nuText = []
-        self.negText = []
-        self.text = []
-
-        self.posReviews = []
-        self.nuReviews = []
-        self.negReviews = []
-
+		# Classify all reviews into arrays
         self.posReviews = self.training[self.training['sentiment'] == 'positive']
         self.nuReviews = self.training[self.training['sentiment'] == 'neutral']
         self.negReviews = self.training[self.training['sentiment'] == 'negative']
 
+        # Get all words from dataset
+        self.posText = [word for tweet in self.posReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
+        self.nuText = [word for tweet in self.nuReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
+        self.negText = [word for tweet in self.negReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
+        self.text = [word for tweet in self.training["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
 
+        # Get all words from selected dataset
         self.selectedPosText = [word for tweet in self.posReviews["selected_text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
         self.selectedNuText = [word for tweet in self.nuReviews["selected_text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
         self.selectedNegText = [word for tweet in self.negReviews["selected_text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
 
-        self.posText = [word for tweet in self.posReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
-        self.nuText = [word for tweet in self.nuReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
-        self.negText = [word for tweet in self.negReviews["text"] for word in list(set(self.vocabulary) & set(self.clean_text(tweet).split()))]
-
-
+        # Get counters for all word in classifications
         self.numReviewsTotal = len(self.training)
         self.probPos = len(self.posReviews) / self.numReviewsTotal
         self.probNu = len(self.nuReviews) / self.numReviewsTotal
@@ -63,61 +56,78 @@ class Sentiment():
         self.nuNum = Counter(self.posText)
         self.negNum = Counter(self.negText)
 
+        # Counters for positive and negative word occurences
+        self.posSelectedNum = Counter(self.selectedPosText)
+        self.nuSelectedNum = Counter(self.selectedPosText)
+        self.negSelectedNum = Counter(self.selectedNegText)
 
-    def classify(self, text, sentiment):
+
+    def extract(self, text, selected_text, sentiment):
         # For each word in the cleaned text of the review
         if (sentiment == "positive"):
-            print("positive")
+            pass
+    #       print("positive")
+
         elif (sentiment == "neutral"):
-            print("neutral")
-        else:
+            #            print("neutral")
+            return text
 
+        else:
             print("negative")
+            print("Original text:", text)
+            print("Selected text:", selected_text)
+            maxVal = (1000, "")
+            for word in self.clean_text(text).split():
+                subVal = self.negSelectedNum[word]/len(self.negText)
+                print(word, subVal)
+                if subVal < maxVal[0] and subVal != 0:
+                    maxVal = (subVal, word)
 
-    def naive_bayes(review, numTotalReviews, probPos, probNu, probNeg, posNum, nuNum, negNum, posText, nuText, negText, vocabulary, alpha):
-        # For each word in the cleaned text of the review
-        pos, nu, neg = 0.0, 0.0, 0.0
-        for word in clean_text(review).split():
-            # getting values for bayes equation
-            vocabLen = len(vocabulary)
+            print(maxVal,"\n" * 3)
+            print("\n" * 3)
+
+    def calculate_selected_text(self, df_row, tol = 0):
+        tweet = df_row['text']
+        sentiment = df_row['sentiment']
+
+        if(sentiment == 'neutral'):
+            return tweet
+
+        elif(sentiment == 'positive'):
+            dict_to_use = pos_words_adj # Calculate word weights using the pos_words dictionary
+
+        elif(sentiment == 'negative'):
+            dict_to_use = neg_words_adj # Calculate word weights using the neg_words dictionary
+
+        words = tweet.split()
+        words_len = len(words)
+        subsets = [words[i:j+1] for i in range(words_len) for j in range(i,words_len)]
+
+        score = 0
+        selection_str = '' # This will be our choice
+        lst = sorted(subsets, key = len) # Sort candidates by length
 
 
-            # Bayes for positivity
-            probWordPos = ((posNum[word] + alpha) / (len(posText) + (vocabLen * alpha)))
-            posNumerator = (probWordPos * probPos)
-            pos += math.log(posNumerator)
+        for i in range(len(subsets)):
 
-            # Bayes for neutrality
-            probWordNu = ((nuNum[word] + alpha) / (len(nuText) + (vocabLen * alpha)))
-            nuNumerator = (probWordNu * probNu)
-            nu += math.log(posNumerator)
+            new_sum = 0 # Sum for the current substring
 
-            # Bayes for negativity
-            probWordNeg = ((negNum[word] + alpha) / (len(negText) + (vocabLen * alpha)))
-            negNumerator = (probWordNeg * probNeg)
-            neg += math.log(negNumerator)
+            # Calculate the sum of weights for each word in the substring
+            for p in range(len(lst[i])):
+                if(lst[i][p].translate(str.maketrans('','',string.punctuation)) in dict_to_use.keys()):
+                    new_sum += dict_to_use[lst[i][p].translate(str.maketrans('','',string.punctuation))]
 
+            # If the sum is greater than the score, update our current selection
+            if(new_sum > score + tol):
+                score = new_sum
+            selection_str = lst[i]
+            #tol = tol*5 # Increase the tolerance a bit each time we choose a selection
 
-            if (DEBUG_BAYES):
-                print("pos numerator", posNumerator)
-                print("pos denominator", posDenominator)
-                print("pos num/den", (posNumerator / posDenominator))
+        # If we didn't find good substrings, return the whole text
+        if(len(selection_str) == 0):
+            selection_str = words
 
-                print("neg numerator", negNumerator)
-                print("neg denominator", negDenominator)
-                print("neg num/den", (negNumerator / negDenominator))
-
-                print("word", word)
-                print("pos:", pos)
-                print("neg:", neg)
-
-        # Return values for positive, nuetral, or negative
-        if (pos > nu) and (pos > neg):
-            return "positive"
-        elif (nu > neg) and (nu > pos):
-            return "neutral"
-        else:
-            return "negative"
+        return ' '.join(selection_str)
 
     def clean_text(self, text):
         # remove HTML tags
@@ -150,9 +160,9 @@ def main():
     sentimentExtract.train()
 
     # Set up training and testing run throughs
-    print(twitter_train["text"][1])
-    sentiment = twitter_train["sentiment"][1]
-    sentimentExtract.classify(twitter_train["text"][1], twitter_train["sentiment"][1])
+    for index, row in twitter_train.iterrows():
+        sentimentExtract.extract(row["text"], row["selected_text"], row["sentiment"])
+        sentimentExtract.calculate_selected_text(row, 0.001)
 
 
 def eval(truth, pred):
@@ -169,6 +179,5 @@ def jaccard(str1, str2):
     c = a.intersection(b)
     return float(len(c)) / (len(a) + len(b) - len(c))
 
-    
 if __name__ == "__main__":
     main()
