245af5952b0450172f486498f76ee52912f56992
Matthew Jordan <matthewsages@gmail.com>
2020-06-16 07:59:11 -0700

    Impliment selected text BERT tokenization

--- main.py
+++ main.py
@@ -42,190 +42,39 @@ class Sentiment():
         model = model_class.from_pretrained(pretrained_weights)
 
         tokenized = self.training["text"].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+        segments = self.training["selected_text"].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
 
-        print(tokenized.str.pad(15, side ='left'))
+        print(self.training["text"][1], tokenized[1])
+        print(self.training["selected_text"][1], segments[1])
 
-        padded = pad_sequences(tokenized)
+        tokenized_ids = tokenized.apply((lambda x: tokenizer.convert_tokens_to_ids(x)))
+        segments_id = segments.apply((lambda x: tokenizer.convert_tokens_to_ids(x)))
 
-        input_ids = torch.tensor(np.array(padded))
+        tokens_tensor = torch.tensor(np.array(pad_sequences(tokenized))).to(torch.int64)
+        segments_tensors = torch.tensor(np.array(pad_sequences(segments))).to(torch.int64)
 
+        # Set the model in evaluation mode to deactivate the DropOut modules
+        # This is IMPORTANT to have reproducible results during evaluation!
+        model.eval()
+
+        # If you have a GPU, put everything on cuda
+        try:
+            tokens_tensor = tokens_tensor.to('cuda')
+            segments_tensors = segments_tensors.to('cuda')
+            model.to('cuda')
+
+        except:
+            print("ERROR: Must have graphics card with CUDA compatibility. Running without GPU acceleration")
+
+        # Predict all tokens
         with torch.no_grad():
-            last_hidden_states = model(input_ids)
+            outputs = model(tokens_tensor, segments_tensors)
+            predictions = outputs[0]
 
-         # Slice the output for the first position for all the sequences, take all hidden unit outputs
-        features = last_hidden_states[0][:,0,:].numpy()
-
-    def calculate_tuples(self, sentiment, dRow):
-        tweets = self.training[self.training['sentiment'] == sentiment]
-        tuples = []
-
-        # Get all words from dataset
-        for tweet in tweets[dRow]:
-                cleanTweet = self.clean_text(tweet, True).split()
-                for index, word in enumerate(cleanTweet):
-                    if word not in self.cv.vocabulary_.keys():
-                        if (index == len(cleanTweet) - 1):
-                            continue
-
-                        #print("Word/clean:", word, cleanTweet[index + 1])
-                        tuples.append((word, cleanTweet[index + 1]))
-
-        return tuples
-
-
-    def train(self):
-        # Get all words from dataset
-        posText = self.training[self.training['sentiment'] == 'positive']
-        nuText = self.training[self.training['sentiment'] == 'neutral']
-        negText = self.training[self.training['sentiment'] == 'negative']
-
-        posNum = self.cv.transform(posText['text'])
-        nuNum = self.cv.transform(nuText['text'])
-        negNum = self.cv.transform(negText['text'])
-
-        posIdfNum = self.idf.transform(posText['text'])
-        nuIdfNum = self.idf.transform(nuText['text'])
-        negIdfNum = self.idf.transform(negText['text'])
-
-        posNum = pd.DataFrame(posNum.toarray(), columns=self.cv.get_feature_names())
-        nuNum = pd.DataFrame(nuNum.toarray(), columns=self.cv.get_feature_names())
-        negNum = pd.DataFrame(negNum.toarray(), columns=self.cv.get_feature_names())
-
-        posIdfNum = pd.DataFrame(posIdfNum.toarray(), columns=self.idf.get_feature_names())
-        nuIdfNum = pd.DataFrame(nuIdfNum.toarray(), columns=self.idf.get_feature_names())
-        negIdfNum = pd.DataFrame(negIdfNum.toarray(), columns=self.idf.get_feature_names())
-
-        # Get all words from dataset
-        self.posTuples = self.calculate_tuples("positive", "text")
-        self.nuTuples = self.calculate_tuples("neutral", "text")
-        self.negTuples = self.calculate_tuples("negative", "text")
-
-        # Get all words from selected dataset
-        self.selectedPosTuples = self.calculate_tuples("positive", "selected_text")
-        self.selectedNuTuples = self.calculate_tuples("neutral", "selected_text")
-        self.selectedNegTuples = self.calculate_tuples("negative", "selected_text")
-
-        # Counters for positive and negative word occurences
-        self.posNumTuples = Counter(self.posTuples)
-        self.nuNumTuples = Counter(self.nuTuples)
-        self.negNumTuples = Counter(self.negTuples)
-
-        # Counters for positive and negative word occurences
-        self.posSelectedNumTuples = Counter(self.selectedPosTuples)
-        self.nuSelectedNumTuples = Counter(self.selectedNuTuples)
-        self.negSelectedNumTuples = Counter(self.selectedNegTuples)
-
-
-        self.allTuples = {**self.posNumTuples, **self.nuNumTuples, **self.negNumTuples}
-
-        pos_words = {}
-        neutral_words = {}
-        neg_words = {}
-
-        pos_tuples = {}
-        neutral_tuples = {}
-        neg_tuples = {}
-
-        for k in self.cv.vocabulary_.keys():
-            pos_words[k] = posNum[k].sum()
-            neutral_words[k] = nuNum[k].sum()
-            neg_words[k] = negNum[k].sum()
-
-        for t in self.allTuples:
-            posTuples = self.posSelectedNumTuples[t]
-            nuTuples = self.nuSelectedNumTuples[t]
-            negTuples = self.negSelectedNumTuples[t]
-
-            pos_tuples[t] = posTuples/len(self.posTuples)
-            neutral_tuples[t] = nuTuples/len(self.nuTuples)
-            neg_tuples[t] = negTuples/len(self.negTuples)
-
-        # We need to account for the fact that there will be a lot of words used in tweets of every sentiment.
-        # Therefore, we reassign the values in the dictionary by subtracting the proportion of tweets in the other
-        # sentiments that use that word.
-
-        self.neg_words_adj = {}
-        self.pos_words_adj = {}
-        self.neutral_words_adj = {}
-
-        self.neg_tuples_adj = {}
-        self.pos_tuples_adj = {}
-        self.neutral_tuples_adj = {}
-
-        for key, value in neg_words.items():
-            self.neg_words_adj[key] = neg_words[key] - (neutral_words[key] + pos_words[key])
-
-        for key, value in pos_words.items():
-            self.pos_words_adj[key] = pos_words[key] - (neutral_words[key] + neg_words[key])
-
-        for key, value in neutral_words.items():
-            self.neutral_words_adj[key] = neutral_words[key] - (neg_words[key] + pos_words[key])
-
-        for key, value in neg_tuples.items():
-            self.neg_tuples_adj[key] = neg_tuples[key] - (neutral_tuples[key] + pos_tuples[key])
-
-        for key, value in pos_tuples.items():
-            self.pos_tuples_adj[key] = pos_tuples[key] - (neutral_tuples[key] + neg_tuples[key])
-
-        for key, value in neutral_tuples.items():
-            self.neutral_tuples_adj[key] = neutral_tuples[key] - (neg_tuples[key] + pos_tuples[key])
-
-
-    def calculate_selected_text(self, df_row, tol = 0):
-        tweet = df_row['text']
-        sentiment = df_row['sentiment']
-
-        if(sentiment == 'neutral'):
-            return tweet
-
-        elif(sentiment == 'positive'):
-            dict_to_use = self.pos_words_adj # Calculate word weights using the pos_words dictionary
-            tuple_dict = self.pos_tuples_adj
-
-        elif(sentiment == 'negative'):
-            dict_to_use = self.neg_words_adj # Calculate word weights using the neg_words dictionary
-            tuple_dict = self.neg_tuples_adj
-
-        words = self.clean_text(tweet).split()
-        words_len = len(words)
-        subsets = [words[i:j+1] for i in range(words_len) for j in range(i,words_len)]
-
-        score = 0
-        selection_str = '' # This will be our choice
-        lst = sorted(subsets, key = len) # Sort candidates by length
-
-
-        for i in range(len(subsets)):
-            new_sum = 0 # Sum for the current substring
-
-            # Calculate the sum of weights for each word in the substring
-            if (len(lst[i]) <= 1):
-                for p in range(len(lst[i])):
-                    if(lst[i][p] in dict_to_use.keys()):
-                        new_sum += dict_to_use[lst[i][p]]
-
-            else:
-                for p in range(len(lst[i])):
-                    if(lst[i][p] in dict_to_use.keys()):
-                        new_sum += dict_to_use[lst[i][p]]
-
-                    if (p + 1) >= len(lst[i]):
-                        continue
-
-                    if((lst[i][p],lst[i][p+1]) in tuple_dict.keys()):
-                        new_sum += 0.575 * tuple_dict[(lst[i][p],lst[i][p+1])]
-
-            # If the sum is greater than the score, update our current selection
-            if(new_sum > score + tol):
-                score = new_sum
-                selection_str = lst[i]
-            #tol = tol*5 # Increase the tolerance a bit each time we choose a selection
-
-        # If we didn't find good substrings, return the whole text
-        if(len(selection_str) == 0):
-            selection_str = words
-
-        return ' '.join(selection_str)
+        # confirm we were able to predict 'henson'
+        predicted_index = torch.argmax(predictions[0, masked_index]).item()
+        predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+        assert predicted_token == 'henson'
 
 
     def clean_text(self, text, punc=False):
@@ -291,14 +140,6 @@ def main():
         # print("\n\n\n\n")
         # sum += jaccard(row['selected_text'], prediction)
     # print(((1/len(twitter_train[21984:]))*sum))
-
-
-def eval(truth, pred):
-    n = len(truth)
-    sum = 0.0
-    for i in range(n):
-        sum += jaccard(truth[i], pred[i])
-    return ((1/n)*sum)
 
 
 def jaccard(str1, str2):
